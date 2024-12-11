ba10f77025b25f426ff257461087eddd5be0e15d
SeelyksDev <kevincordier.dev@gmail.com>
2024-12-11 23:32:32 +0100

    feat(photographer.js): create elements to display photographer information on DOM

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,17 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const div = document.createElement('div');
+        div.innerHTML = `<p>${city}, ${country}</p>`
+        const description = document.createElement('p');
+        description.textContent = tagline;
+        const priceDay = document.createElement('p');
+        priceDay.textContent = `${price}€/jour`;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(div);
+        article.appendChild(description);
+        article.appendChild(priceDay);
         return (article);
     }
     return { name, picture, getUserCardDOM }

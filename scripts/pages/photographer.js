78e45af7cac87cb6ea338ee88b7f829310f09f6c
SeelyksDev <kevincordier.dev@gmail.com>
2024-12-28 10:59:43 +0100

    refactor(photographer.js): implement factory pattern for gallery template

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -15,86 +15,42 @@ async function getPhotographerById(id) {
     return { photographerDetails, photographerMedia };
 }
 
-async function displayPhotographerDetails() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const photographerId = urlParams.get("id");
-    const toNumberId = Number(photographerId);
-
-    if (photographerId) {
-        const { photographerDetails } = await getPhotographerById(toNumberId);
+async function displayPhotographerDetails(id) {
+    if (id) {
+        const { photographerDetails } = await getPhotographerById(id);
 
         if (photographerDetails) {
-            const photographHeader =
-                document.querySelector(".photograph-header");
-
-            photographHeader.innerHTML = `
-            <div class="photograph-header-informations">
-                <h1 class="photograph-name">${photographerDetails.name}</h1>
-                <address class="photograph-location">
-                    <p class="photograph-city">${photographerDetails.city},</p>
-                    <p class="photograph-country">${
-                        photographerDetails.country
-                    }</p>
-                </address>
-                <p class="photograph-description">${
-                    photographerDetails.tagline
-                }</p>
-            </div>
-            <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
-            <img class="photograph-img" src="./assets/photographers/${
-                photographerDetails.portrait
-                    ? photographerDetails.portrait
-                    : account.png
-            }" alt=${photographerDetails.name}></img>
-        `;
-
-            const pricePerDay = document.querySelector(".price-per-day");
-            pricePerDay.innerHTML = `<p class="price">${photographerDetails.price}€ / jour</p>`;
+            const photographer = photographerTemplate(photographerDetails);
+            return photographer.getDetailsDOM();
         }
     }
 }
 
-async function displayGalleryWorks() {
+async function displayGalleryWorks(id) {
+    if (id) {
+        const { photographerMedia, photographerDetails } =
+            await getPhotographerById(id);
+
+        if (photographerMedia && photographerDetails) {
+            const sortedMedia = photographerMedia.sort((a, b) => b.likes - a.likes);
+            
+            sortedMedia.forEach((photographMedia) => {
+                const galleryModel = galleryTemplate(photographMedia, photographerDetails);
+                galleryModel.getGalleryDOM();
+            });
+        }
+    }
+}
+
+function initPhotographerDetails() {
     const urlParams = new URLSearchParams(window.location.search);
     const photographerId = urlParams.get("id");
     const toNumberId = Number(photographerId);
 
-    if (photographerId) {
-        const { photographerMedia, photographerDetails } =
-            await getPhotographerById(toNumberId);
-
-        if (photographerMedia) {
-            const workGallery = document.querySelector(".work-gallery");
-            photographerMedia.map(
-                (media) =>
-                    (workGallery.innerHTML += `
-                <article class="work-card">
-                 ${
-                     media.image
-                         ? `<img src="./assets/images/${photographerDetails.name}/${media.image}" alt="${media.title}" class="card-media">`
-                         : ""
-                 }
-                 ${
-                     media.video
-                         ? `<video controls class="card-media">>
-                    <source src="./assets/images/${photographerDetails.name}/${media.video}" type="video/mp4"/>
-                    </video>`
-                         : ""
-                 }
-                    <div class="card-description">
-                        <p class="card-title">${media.title}</p>
-                        <div class="card-likes-stats">
-                            <p class="number-likes">${media.likes}</p>
-                            <img src="./assets/icons/red-heart.svg" alt="image d'un coeur rouge" class="card-heart">
-                        </div>
-                    </div>
-                </article>
-                `)
-            );
-            
-        }
+    if (toNumberId) {
+        displayPhotographerDetails(toNumberId);
+        displayGalleryWorks(toNumberId);
     }
 }
 
-displayPhotographerDetails();
-displayGalleryWorks();
+initPhotographerDetails();
